b7d84df66542c5143d542400d757dc9f41c0b82b
Christian Aichner <contact@aichner-christian.com>
2019-07-26 23:21:39 +0200

    add dynamic adding

--- src/components/waves/organisms/Tabs/Generic/defineFields.jsx
+++ src/components/waves/organisms/Tabs/Generic/defineFields.jsx
@@ -1,6 +1,9 @@
 // React
 import React from 'react'
 
+// React addons
+import update from 'react-addons-update'; // ES6
+
 // MDB
 import {
     MDBBtn,
@@ -39,7 +42,7 @@ class DefFields extends React.Component{
     addFieldDialog = () => {
         // Get current fields
         let fielddialogs = this.state.fielddialogs;
-        let newfield = { method: 'default', field: '', type: '', weight: ''};
+        let newfield = { status: 'dialog', field: '', type: '', weight: ''};
         // Add new field to current fields
         fielddialogs.push(newfield);
         // Add current fields + new field to state
@@ -68,52 +71,89 @@ class DefFields extends React.Component{
         // Because we used splice and did not call this.setState() we need to manually update (and call the component's render function)
         this.forceUpdate(); // This is as dirty as it gets! -> Should be avoided if possible!
     }
-   
+
+    // Add dialog
+    addDialog = (e) => {
+        // Receive field value of dialog
+        let value = e.target.getAttribute('data');
+        if(value !== undefined){
+            // Get position
+            let pos = this.state.fielddialogs.map(function(e) { return e.field }).indexOf(value);
+            // Update state
+            this.setState({
+                fielddialogs: update(this.state.fielddialogs, {[pos]: {status: {$set: 'set'}}})
+            }, () => console.log(this.state))
+        }
+    }
+
+    // Remove dialog
+    removeDialog = (e) => {
+        // To be added
+    }
+
     render(){
         return(
             <div className="row">
                 <div className="col-md-6">
-                    
-                    <form>
-                        {this.state.fielddialogs.map((dialog, index) => {
+                    {this.state.fielddialogs.map((dialog, index) => {
+                        if(dialog.status === 'set'){
                             return(
                                 <FadeIn key={index}>
-                                    <div className="card p-3 mb-3">
-                                        <div className="row">
-                                            <div className="col-md-6">
-                                                <label>Field</label>
-                                                <input
-                                                    type="text"
-                                                    name="field"
-                                                    value={dialog.field}
-                                                    onChange={(e) => {
-                                                        const newPeople = this.state.fielddialogs.slice();
-                                                        newPeople[index].field = e.target.value;
-                                                        this.setState({ fielddialogs: newPeople });
-                                                    }}
-                                                    className="form-control"
-                                                />
-                                            </div>
-                                            <div className="col-md-6">
-                                                <label>Type</label>
-                                                <select name="type" className="browser-default custom-select" index={index} onChange={this.handleChange.bind(this)} >
-                                                    <option value="sel_num">Number</option>
-                                                    <option value="sel_slider">Slider</option>
-                                                    <option value="sel_checkbox">Checkbox</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-md-6 text-left align-self-center">
-                                                <MDBBtn size="md" color="elegant" outline data={dialog.field} onClick={this.cancelDialog.bind(this)}><MDBIcon icon="minus" className="pr-2" />Cancel</MDBBtn>
-                                            </div>
-                                            <div className="col-md-6 text-right">
-                                                <MDBBtn color="deep-orange"><MDBIcon icon="plus" className="pr-2" />Add</MDBBtn>
-                                            </div>
-                                        </div>
+                                    <div>
+                                        {dialog.field}
                                     </div>
                                 </FadeIn>
                             );
+                        } else {
+                            return null;
+                        }
+                        
+                    })}
+                    <form>
+                        {this.state.fielddialogs.map((dialog, index) => {
+                            if(dialog.status === 'dialog'){
+                                return(
+                                    <FadeIn key={index}>
+                                        <div className="card p-3 mb-3">
+                                            <div className="row">
+                                                <div className="col-md-6">
+                                                    <label>Field</label>
+                                                    <input
+                                                        type="text"
+                                                        name="field"
+                                                        value={dialog.field}
+                                                        onChange={(e) => {
+                                                            const newPeople = this.state.fielddialogs.slice();
+                                                            newPeople[index].field = e.target.value;
+                                                            this.setState({ fielddialogs: newPeople });
+                                                        }}
+                                                        className="form-control"
+                                                    />
+                                                </div>
+                                                <div className="col-md-6">
+                                                    <label>Type</label>
+                                                    <select name="type" className="browser-default custom-select" index={index} onChange={this.handleChange.bind(this)} >
+                                                        <option value="sel_num">Number</option>
+                                                        <option value="sel_slider">Slider</option>
+                                                        <option value="sel_checkbox">Checkbox</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div className="row">
+                                                <div className="col-md-6 text-left align-self-center">
+                                                    <MDBBtn size="md" color="elegant" outline data={dialog.field} onClick={this.cancelDialog.bind(this)}><MDBIcon icon="minus" className="pr-2" />Cancel</MDBBtn>
+                                                </div>
+                                                <div className="col-md-6 text-right">
+                                                    <MDBBtn color="deep-orange" data={dialog.field} onClick={this.addDialog.bind(this)}><MDBIcon icon="plus" className="pr-2" />Add</MDBBtn>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </FadeIn>
+                                );
+                            } else {
+                                return null;
+                            }
+                            
                         })}
                     </form>
                   
