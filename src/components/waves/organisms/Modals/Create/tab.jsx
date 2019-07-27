8f52ec96018afd89c160311fdb539b4081a41831
Christian Aichner <contact@aichner-christian.com>
2019-07-27 17:02:31 +0200

    add smart icons

--- src/components/waves/organisms/Modals/Create/tab.jsx
+++ src/components/waves/organisms/Modals/Create/tab.jsx
@@ -29,8 +29,96 @@ class CreateTab extends React.Component {
         this.setState({[e.target.name]: e.target.value});
     }
 
-    handleSubmit = (e) => {
-        e.preventDefault();
+    word_iconallySetIcon = () => {
+        let icons = [];
+        // Get title of the tab and serialize it
+        const title = this.state.title.toLowerCase().trim();
+        // Split the title in all words
+        let words = title.split(" ");
+        words.map((word, i) => {
+            switch(word){
+                case 'work':
+                    icons.push("briefcase");
+                    break;
+                case 'sleep':
+                    icons.push("bed");
+                    break;
+                case 'food' || 'breakfast' || 'lunch' || 'dinner':
+                    icons.push("utensils");
+                    break;
+                case 'sex':
+                    icons.push("heart");
+                    break;
+                case 'sports' || 'activities':
+                    icons.push("running");
+                    break;
+                case 'workout':
+                    icons.push("dumbbell");
+                    break;
+                case 'money' || 'money spent':
+                    icons.push("hand-holding-usd");
+                    break;
+                case 'meditation':
+                    icons.push("leaf");
+                    break;
+                case 'school' || 'education' || 'university' || 'college':
+                    icons.push("graduation-cap");
+                    break;
+                case 'car':
+                    icons.push("car");
+                    break;
+                case 'hiking' || 'walk' || 'walking':
+                    icons.push("hiking");
+                    break;
+                case 'transportation' || 'transport':
+                    icons.push("bus");
+                    break;
+                case 'shower':
+                    icons.push("shower");
+                    break;
+                case 'water':
+                    icons.push("water");
+                    break;
+                case 'shit' || 'toilet' || 'poo' || 'poop':
+                    icons.push("poo-storm");
+                    break;
+                case 'coffee':
+                    icons.push("coffee");
+                    break;
+                case 'tee':
+                    icons.push("mug-hot");
+                    break;
+                case 'sweets' || 'cookies' || 'chocolate':
+                    icons.push("coookie");
+                    break;
+                case 'reading' || 'books' || 'read' || 'book':
+                    icons.push("book-open");
+                    break;
+                case 'audible':
+                    icons.push("audible");
+                    break;
+                case 'cat' || 'meow' || 'mau' || 'miau' || 'maua':
+                    icons.push('cat');
+                    break;
+                case 'dog' || 'wouf' || 'wuff' || 'wau' || 'wow':
+                    icons.push('dog');
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        })
+
+        console.log(icons);
+        if(icons.length === 0){
+            icons.push('file-alt');
+        }
+
+        // Set icon and send form
+        this.setState({icon: icons[0]}, () => this.sendForm())
+    }
+
+    sendForm = () => {
         if(this.state.title.trim() !== ""){
             // Remove overhead (this.state.error) that otherwise would end up in the DB
             let cleaned = {
@@ -46,6 +134,13 @@ class CreateTab extends React.Component {
         } else {
             this.setState({error: "Please enter a name for your tab."}, () => this.props.onSendForm(this.state))
         }
+    }
+
+    handleSubmit = (e) => {
+        // Prevent site from refreshing
+        e.preventDefault();
+        // Set icon (if tite is a switch case)
+        this.word_iconallySetIcon();
     }
 
     // Color picker
